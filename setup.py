b4df8cd7566658f6599c508aaf9e79c264950880
khmurakami <kalanimurakami1218@gmail.com>
2020-01-02 12:29:40 -0800

    created template for setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="printfulpy",
+    version="0.0.3",
+    author="Kalani Murakami",
+    author_email="kalanimurakami1218@gmail.com",
+    description="Unofficial Printful Python API",
+    packages=['printfulpy'],
+    install_requires=["request", "wget"],
+    license="MIT",
+    url="https://github.com/khmurakami/printfulpy"
+)
